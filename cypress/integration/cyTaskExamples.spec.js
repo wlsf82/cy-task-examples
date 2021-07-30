2bc48ca1f3503ed21df536a12f10050f42e2b8cc
Walmyr <wlsf82@gmail.com>
2021-07-31 00:19:25 +0200

    Hack it better

--- cypress/integration/cyTaskExamples.spec.js
+++ cypress/integration/cyTaskExamples.spec.js
@@ -87,11 +87,11 @@ describe('cy.task("examples")', () => {
   it('hack the page which makes the test fail', () => {
     // https://docs.cypress.io/api/commands/visit#Window
     cy.visit('https://www.saucedemo.com/').then(win => {
-      win.document.querySelector('.login_password').innerHTML = `\n<h4>Password for all users:</h4><p>hacked_sauce</p>`
+      win.document.querySelector('.login_password').innerHTML = `\n<h4>Password for all users:</h4>hacked_sauce`
     })
 
-    cy.get('.login_password p').then(el => {
-      const password = el[0].innerText
+    cy.get('.login_password').then(el => {
+      const password = el[0].innerText.replace('Password for all users:', '')
 
       cy.get('[data-test=username]').type('standard_user')
       cy.get('[data-test=password]').type(password)
